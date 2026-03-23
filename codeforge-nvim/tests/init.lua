1de492555aae68848dc9b66099b0f790d2df5886
ChosunOne <chosunone@protonmail.com>
2026-03-23 18:18:39 +0900

    test: work on tests

--- codeforge-nvim/tests/init.lua
+++ codeforge-nvim/tests/init.lua
@@ -1,7 +1,18 @@
 vim.o.runtimepath = vim.fn.getcwd() .. "," .. vim.o.runtimepath
 
 local mini_path = vim.fn.stdpath("data") .. "/lazy/mini.nvim"
-vim.o.runtimepath = vim.o.runtimepath .. "," .. mini_path
+local dapui_path = vim.fn.stdpath("data") .. "/lazy/nvim-dap-ui"
+local nvim_dap_path = vim.fn.stdpath("data") .. "/lazy/nvim-dap"
+local nio_path = vim.fn.stdpath("data") .. "/lazy/nvim-nio"
+vim.o.runtimepath = vim.o.runtimepath
+	.. ","
+	.. mini_path
+	.. ","
+	.. dapui_path
+	.. ","
+	.. nvim_dap_path
+	.. ","
+	.. nio_path
 
 require("mini.test").setup()
 require("codeforge").setup()
